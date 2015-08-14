18f1e20479b5b1255fa7e2f0ddaba48a126d8ae2
Xinyu <xyzhang.me@aliyun.com>
2015-08-14 16:31:04 +0800

    fix bug in cacheSrcStorage, add set/get

--- cache-src.js
+++ cache-src.js
@@ -233,11 +233,18 @@
         };
     });
 
-
     module.factory('cacheSrcStorage', function($localStorage) {
-        return $localStorage.cache_src;
+        var c = {};
+        c._cache = $localStorage.cache_src;
+        c.get = function(url){
+            return c._cache[url] && getCacheDir() + c._cache[url];
+        };
+        c.set = function(url,localUrl){
+            c._cache[url] = localUrl;
+            return c;
+        };
+        return c;
     });
-
 
 
     module.directive('cacheSrc', cacheSrc);
